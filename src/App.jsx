517897316cc56f3e3fb7b540b276dccd484dfe53
unknown <aisyahputri.w1@gmail.com>
2024-10-11 18:22:04 +0700

    coba baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() { 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>Aisyah</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+	  <>
+	    <h1 className="text-3xl font-bold">
+	      ini font body
+	    </h1>
+      <h1 className="text-3xl font-poppins text-purple-600">
+        ini poppins 
+      </h1>
+      <h1 className="text-3xl"> ini font body lagi</h1>
+      <h1><p> halo,ini aisyah</p></h1>
+	  </>
   )
 }
 
