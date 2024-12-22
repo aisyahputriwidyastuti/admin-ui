f73e871f6f2318ea6f9872d7165eff994308a1d6
unknown <aisyahputri.w1@gmail.com>
2024-12-22 13:08:50 +0700

    validate with react hook form

--- src/components/Elements/LabeledInput/index.jsx
+++ src/components/Elements/LabeledInput/index.jsx
@@ -1,15 +1,14 @@
-import Input from "./Input";
-import Label from "./Label";
-
-const LabeledInput = (props) => {
-  const { label, name, type, placeholder } = props;
+const Input = ({ name, type, placeholder, register}) => {
+  const variant = "bg-special-mainBg border border-gray-03";
 
   return (
-    <>
-      <Label htmlFor={name}>{label}</Label>
-      <Input name={name} type={type} placeholder={placeholder} />
-    </>
+    <input 
+    type={type}
+    className={`${variant} py-3 ps-4 text-sm rounded-md w-full text-gray-01 focus:border-black`}
+    placeholder={placeholder}
+    name={name}
+    id={name}
+    {...register}/>
   );
 };
-
-export default LabeledInput;
+export default Input;
--- src/components/Fragments/FormSignIn.jsx
+++ src/components/Fragments/FormSignIn.jsx
@@ -1,17 +1,38 @@
 import Button from "../Elements/Button";
 import CheckBox from "../Elements/CheckBox";
 import LabeledInput from "../Elements/LabeledInput";
+import { useForm } from "react-hook-form";
 
 const FormSignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid }, // Ambil errors dari formState
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onFormSubmit = (data) => console.log(data);
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onFormSubmit)}>
       <div className="mb-6">
         <LabeledInput
           label="Email address"
           type="email"
           placeholder="hello@example.com"
           name="email"
+          register={{
+            ...register("email", {
+              required: "Email address is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address format",
+              },
+            }),
+          }}
         />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>} {/* Tampilkan error */}
       </div>
       <div className="mb-6">
         <LabeledInput
@@ -19,12 +40,25 @@ const FormSignIn = () => {
           type="password"
           placeholder="*************"
           name="password"
+          register={{
+            ...register("password", {
+              required: "Password is required",
+            }),
+          }}
         />
+        {errors.password && <p className="text-red-500">{errors.password.message}</p>} {/* Tampilkan error */}
       </div>
       <div className="mb-3">
         <CheckBox label="Keep me signed in" name="status" />
       </div>
-      <Button variant="bg-primary w-full text-white" type="submit">
+      <Button variant={
+        !isValid
+        ? "bg-gray-05 w-full text-white"
+        : "bg-primary w-full text-white"
+        } 
+        type="submit"
+        disabled={!isValid ? "disabled" : ""}
+        >
         Login
       </Button>
     </form>
